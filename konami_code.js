d1b0067aea12d5d18873b241743901508f581fbd
Greg Morgan <gregm9999@gmail.com>
2019-01-19 23:55:03 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index=0;
+document.addEventListener("keydown", init);
 function init(e) {
   // Write your JavaScript code inside the init() function
   console.log(e);
