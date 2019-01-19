2443e10d82b66feeb13bb98d94555ebd20f1eded
Greg Morgan <gregm9999@gmail.com>
2019-01-19 23:52:11 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,6 +2,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index=0;
 function init(e) {
   // Write your JavaScript code inside the init() function
+  console.log(e);
+  console.log(e.detail);
+  console.log(e.which);
+  console.log(e.location);
   const key = parseInt(e.detail||e.which, 10);
   if (key===code[index]) {
     index++;
