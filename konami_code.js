0386e050d2e862fd3734e65f5ce485970f400832
Greg Morgan <gregm9999@gmail.com>
2019-01-20 00:46:09 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,21 +1,17 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  let index = 0;
-
-  document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.which || e.detail);
-
-    if (code[index] === key) {
+  let index=0;
+  document.body.addEventListener("keydown", function (e) {
+    let key=parseInt(e.which, 10);
+    if (code[index]===key){
       index++;
-
-      if (index === code.length) {
-        alert('Hurray!');
-
-        index = 0;
+      if(index===code.length) {
+        alert("Konami Code Activated!!!");
+        index=0;
       }
-    } else {
-      index = 0;
+    }  else {
+      index=0;
     }
   });
 }
