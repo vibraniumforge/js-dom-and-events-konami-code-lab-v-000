4021f7b2470884eaefd0eace5c8f524ffd95c26a
Greg Morgan <gregm9999@gmail.com>
2019-01-20 00:51:28 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index=0;
   document.body.addEventListener("keydown", function (e) {
-    const key=parseInt(e.which, 10);
+    const key=parseInt(e.which||e.detail);
     if (code[index]===key){
       index++;
       if(index===code.length) {
